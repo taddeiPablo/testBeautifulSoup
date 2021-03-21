83dd359adfa9469699c1bb32a831e9f3e335447f
pablo <taddeiApablo23@gmail.com>
2021-03-21 05:55:44 -0300

    continuar ya casi lo tenemos
    continuar ya casi lo tenemos

--- ejemplo/emojiDownload.py
+++ ejemplo/emojiDownload.py
@@ -20,18 +20,19 @@ class EmojisDownload(object):
         self.path_emoti = None
     def load_file(self):
         self.path = os.path.dirname(os.path.abspath(__file__)) + '/file/emo.csv'
-        self.path_emoti = os.path.dirname(os.path.abspath(__file__))
+        self.path_emoti = os.path.dirname(os.path.abspath(__file__)) # aqui el path en donde pretendo guardar los emoticones
         with open(self.path, newline='') as csvfile:
             reader = csv.reader(csvfile, delimiter=';', quoting=csv.QUOTE_NONE)
             for row in reader:
-                print(self.path_emoti + '\\' + row[0])
-                print(os.chdir(self.path_emoti + '\\' + row[0]))
-                '''if os.chdir(self.path_emoti + '/' + row[0]):
-                    print("existe el directorio")
-                else:
-                    print("el directorio no existe")'''
-                    
-    def result_search(self, search):
+                if row[0] != 'filename':
+                    parent_dir = self.path_emoti + '\\'
+                    if os.path.isdir(self.path_emoti + '\\' + row[0]) == False:
+                        path = os.path.join(parent_dir, row[0]) 
+                        os.mkdir(path)
+                        # aqui llamamos a result_search y le pasamos la url y el path
+                    else
+                        # lo mismo 
+    def result_search(self, search, path=""):
         self.agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
         self.url = search
         self.contents = requests.get(self.url, headers=self.agent).text#urllib.urlopen(self.url).read()
@@ -43,7 +44,6 @@ class EmojisDownload(object):
             aux = its.find_all("a", class_="view link-icon-detail")
             if len(aux) != 0:
                 print(aux[0].img.attrs['data-src'])
-
 
 if __name__== "__main__":
     '''#Ml = MercadoLibre()
