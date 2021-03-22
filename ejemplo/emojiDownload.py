8bb3b6c634ad4fcb47c927830f0abf69c50be2da
pablo <taddeiApablo23@gmail.com>
2021-03-22 18:22:32 -0300

    terminado el script
    terminado este script listo para ser usado

--- ejemplo/emojiDownload.py
+++ ejemplo/emojiDownload.py
@@ -4,12 +4,18 @@
 
 from bs4 import BeautifulSoup
 import urllib
+import urllib.request
 import requests
 import json
 import io
 import os
 import csv
 
+
+###
+#
+#
+###
 class EmojisDownload(object):
     def init (self):
         self.contents = ""
@@ -31,34 +37,25 @@ class EmojisDownload(object):
                     if os.path.isdir(self.path_emoti + '\\' + row[0]) == False:
                         self.path_dir = os.path.join(parent_dir, row[0]) 
                         os.mkdir(self.path_dir)
-                        # aqui llamamos a result_search y le pasamos la url y el path
+                        self.result_search(row[1], self.path_dir)
                     else:
                         self.path_dir = os.path.join(self.path_emoti + '\\', row[0])
                         self.result_search(row[1], self.path_dir)
     def result_search(self, search, path_dir):
-        print(search)
-        print(path_dir)
         self.agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
         self.url = search
-        self.contents = requests.get(self.url, headers=self.agent).text#urllib.urlopen(self.url).read()
+        self.contents = requests.get(self.url, headers=self.agent).text #urllib.urlopen(self.url).read()
         self.beauti = BeautifulSoup(self.contents, 'lxml')
         self.items = self.beauti.findAll('ul', attrs={'class': 'icons'})
-        self.scrape_page_items(self.items)
-    def scrape_page_items(self, ite):
+        self.scrape_page_items(self.items, path_dir)
+    def scrape_page_items(self, ite, filePath):
         for its in ite[0].find_all('li'):
             aux = its.find_all("a", class_="view link-icon-detail")
             if len(aux) != 0:
                 print(aux[0].img.attrs['data-src'])
-
+                title = aux[0].img.attrs['title']
+                urllib.request.urlretrieve(aux[0].img.attrs['data-src'], filePath + '\\' + title + '.png')
 if __name__== "__main__":
-    '''#Ml = MercadoLibre()
-    #Ol = Olx()
-    EM = EmojisDownload()
-    print("bienvenido a esta prueba")
-    busqueda = input('ingrese la busqueda :')
-    EM.result_search(busqueda)
-    #Ml.result_search(busqueda)
-    #Ol.result_search(busqueda)'''
     EM = EmojisDownload()
     EM.load_file()
     
