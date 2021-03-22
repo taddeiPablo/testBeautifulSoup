f8029ca902a077e9ce8d57d4870e10fa11a99cde
pablo <taddeiApablo23@gmail.com>
2021-03-22 00:13:25 -0300

    continuar con este desarrollo
    continuar con este script

--- ejemplo/emojiDownload.py
+++ ejemplo/emojiDownload.py
@@ -18,6 +18,8 @@ class EmojisDownload(object):
         self.agent = None
         self.path = None
         self.path_emoti = None
+        self.path_dir = None
+        self.parent_dir = None
     def load_file(self):
         self.path = os.path.dirname(os.path.abspath(__file__)) + '/file/emo.csv'
         self.path_emoti = os.path.dirname(os.path.abspath(__file__)) # aqui el path en donde pretendo guardar los emoticones
@@ -27,12 +29,15 @@ class EmojisDownload(object):
                 if row[0] != 'filename':
                     parent_dir = self.path_emoti + '\\'
                     if os.path.isdir(self.path_emoti + '\\' + row[0]) == False:
-                        path = os.path.join(parent_dir, row[0]) 
-                        os.mkdir(path)
+                        self.path_dir = os.path.join(parent_dir, row[0]) 
+                        os.mkdir(self.path_dir)
                         # aqui llamamos a result_search y le pasamos la url y el path
-                    else
-                        # lo mismo 
-    def result_search(self, search, path=""):
+                    else:
+                        self.path_dir = os.path.join(self.path_emoti + '\\', row[0])
+                        self.result_search(row[1], self.path_dir)
+    def result_search(self, search, path_dir):
+        print(search)
+        print(path_dir)
         self.agent = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
         self.url = search
         self.contents = requests.get(self.url, headers=self.agent).text#urllib.urlopen(self.url).read()
